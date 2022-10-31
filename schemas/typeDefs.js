3b69d1b0b687875da1f623727bd5f75607199e32
Matt Luna <mat.c.luna@gmail.com>
2022-10-31 00:18:49 -0500

    setup the GraphQL using Apollo. Worked on setting the resolvers and the typeDefs as well as play with the faker.js seed info. The queries are defined on the resolvers to show what can be returned in a query, and mirror what the query will look like in Apollo,  and the typeDef define the data that will be passed.

---
+++ schemas/typeDefs.js
@@ -0,0 +1,42 @@
+// import tye gql tagged template function
+const { gql } = require('apollo-server-express');
+
+// create typdefs
+
+const typeDefs = gql`
+  type Thought {
+    _id: ID
+    thoughtText: String
+    createdAt: String
+    username: String
+    reactionCount: Int
+    reactions: [Reaction]
+  }
+
+  type Reaction {
+    _id: ID
+    reactionBody: String
+    createdAt: String
+    username: String
+  }
+
+  type User {
+    _id: ID
+    username: String
+    email: String
+    company: String
+    friendCount: Int
+    thoughts: [Thought]
+    friends: [User]
+  }
+
+  type Query {
+    users: [User]
+    user(username: String!): User
+    thoughts(username: String): [Thought]
+    thought(_id: ID!): Thought
+  }
+`;
+
+// export typedefs
+module.exports = typeDefs;
