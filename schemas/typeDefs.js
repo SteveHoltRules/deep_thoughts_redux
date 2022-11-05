fa325cf728f463945c1ad8308a1a50ef024038d4
Matt Luna <mat.c.luna@gmail.com>
2022-11-05 07:33:56 -0500

    half way through 21.2.5

--- schemas/typeDefs.js
+++ schemas/typeDefs.js
@@ -4,6 +4,15 @@ const { gql } = require('apollo-server-express');
 // create typdefs
 
 const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    friendCount: Int
+    thoughts: [Thought]
+    friends: [User]
+  }
+
   type Thought {
     _id: ID
     thoughtText: String
@@ -20,21 +29,22 @@ const typeDefs = gql`
     username: String
   }
 
-  type User {
-    _id: ID
-    username: String
-    email: String
-    company: String
-    friendCount: Int
-    thoughts: [Thought]
-    friends: [User]
-  }
-
   type Query {
+    me: User
     users: [User]
     user(username: String!): User
     thoughts(username: String): [Thought]
     thought(_id: ID!): Thought
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+  }
+
+  type Auth {
+    token: ID!
+    user: User
   }
 `;
 
